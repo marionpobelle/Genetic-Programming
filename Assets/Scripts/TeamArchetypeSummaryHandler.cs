799246160b3b576391fca0d49b861ba26bcf9f68
Louis <>
2024-04-04 09:57:36 +0200

    add tmpro

---
+++ Assets/Scripts/TeamArchetypeSummaryHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamArchetypeSummaryHandler : MonoBehaviour
+{
+    [SerializeField] TeamArchetypeUI ui;
+
+    public void SetupUI(int teamAmount)
+    {
+
+    }
+}
